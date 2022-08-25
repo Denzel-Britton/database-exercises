e496cc06bb4292989a9cb7382ddf60cc9ec429ed
Denzel-Britton <denzel.britton573@gmail.com>
2022-08-25 16:49:33 -0500

    finished the delete exercise

---
+++ select_exercises.sql
@@ -0,0 +1,14 @@
+SELECT name  FROM albums WHERE artist = 'Pink Floyd';
+
+SELECT release_date FROM alumbs WHERE name  = 'Sgt. Pepper''s Lonely Hearts Club Band';
+
+SELECT genre FROM albums Where name = 'Nevermind';
+
+SELECT name FROM albums Where release_date  BETWEEN 1990 AND 1999;
+
+
+SELECT name FROM albums WHERE sales < 20.0;
+
+SELECT name FROM albums WHERE genre = 'rock';
+
+SELECT * FROM albums WHERE id = 1
