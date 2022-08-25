004288f68aa8bfbbed855ce825dd176688cc6b22
Denzel-Britton <denzel.britton573@gmail.com>
2022-08-25 11:36:16 -0500

    got all the code for mySql

---
+++ albums_migration.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS quotes (
+    ...
+);
+CREATE TABLE albums (
+                        id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                        artist VARCHAR(50) DEFAULT 'NONE',
+                        song_name  VARCHAR(100) NOT NULL,
+                        release_date INTEGER,
+                        sales FLOAT,
+                        genre VARCHAR(50),
+                        PRIMARY KEY (id)
+);
+-- //id — auto-incrementing unsigned integer primary key
+-- artist — string for storing the recording artist name
+-- name — string for storing a record name
+-- release_date — integer representing year record was released
+-- sales — floating point value for number of records sold (in millions)
+-- genre — string for storing the record's genre(s)
